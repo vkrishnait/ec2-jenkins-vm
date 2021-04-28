ebf30a1653456fd301a820c1e0e63bfe32cfd18c
vkrishnait <47443290+vkrishnait@users.noreply.github.com>
2021-04-28 10:42:35 +0530

    Update userdata.sh

--- userdata.sh
+++ userdata.sh
@@ -11,4 +11,3 @@ sudo apt-get update && sudo apt-get install -y gnupg software-properties-common 
 curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
 sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
 sudo apt-get -y update && sudo apt-get -y install terraform
-
